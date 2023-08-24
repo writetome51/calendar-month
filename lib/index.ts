f21c5bfeced488c6b4840eb02c2a65364ddb5ecf
Steve Thompson <writetome51@gmail.com>
2023-08-24 00:25:50 -0600

    minor format change

--- lib/index.ts
+++ lib/index.ts
@@ -30,11 +30,11 @@ export type CalendarMonthData = Required<CalendarMonthSettings> & {
      * Includes for display the days of previous and next months.
      * I.E., This is a February whose first day is a Wednesday (and the week begins on Sunday):
      [
-     [29,30,31,1,2,3,4],
-     [5,6,7,8,9,10,11],
-     [12,13,14,15,16,17,18],
-     [19,20,21,22,23,24,25],
-     [26,27,28,1,2,3,4]
+       [29,30,31,1,2,3,4],
+       [5,6,7,8,9,10,11],
+       [12,13,14,15,16,17,18],
+       [19,20,21,22,23,24,25],
+       [26,27,28,1,2,3,4]
      ]
      *****/
     weeks: number[][];
